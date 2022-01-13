57d53ab815674ab9f14320356eb151cb86f1effb
Felix Freidman <felixfreidman@gmail.com>
2022-01-13 15:14:35 +0800

    3:14PM Commit New Redisign Match

--- src/assets/js/components/components.js
+++ src/assets/js/components/components.js
@@ -1,27 +1,45 @@
-// // header-swiper
-// var swiper = new Swiper('#main-swiper', {
-//   fadeEffect: {
-//     crossFade: true
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next--header',
-//     prevEl: '.swiper-button-prev--header',
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//     type: 'bullets',
-//     clickable: true,
-//   },
-//   loop: true,
-//   // autoplay: {
-//   //   delay: 2300,
-//   // },
-//   fadeEffect: {
-//     crossFade: true
-//   },
-//   speed: 800,
-//   watchSlidesProgress: true,
-//   watchVisibility: true,
-//   disableOnInteraction: true,
-// });
+// header-swiper
+function swiperInit() {}
+var swiper = new Swiper("#bankSwiper", {
+    fadeEffect: {
+        crossFade: true,
+    },
+    navigation: {
+        nextEl: ".bank-next",
+        prevEl: ".bank-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+    },
+    loop: true,
+    fadeEffect: {
+        crossFade: true,
+    },
+    speed: 800,
+    watchSlidesProgress: true,
+    watchVisibility: true,
+});
 
+var swiper = new Swiper("#whyusSwiper", {
+    fadeEffect: {
+        crossFade: true,
+    },
+    navigation: {
+        nextEl: ".whyus-next",
+        prevEl: ".whyus-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+    },
+    loop: true,
+    fadeEffect: {
+        crossFade: true,
+    },
+    speed: 800,
+    watchSlidesProgress: true,
+    watchVisibility: true,
+});
